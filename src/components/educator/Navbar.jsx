35051eea474202f0b1e71d7d2cc16f39fed52333
Harsimran Kaur <sunshinelife067@gmail.com>
2026-06-22 15:14:11 +0530

    feat: create navbar for educator pages

--- src/components/educator/Navbar.jsx
+++ src/components/educator/Navbar.jsx
@@ -1,9 +1,21 @@
-const Navbar = () => {
-  return (
-    <div>
-      <h1>Navbar Educator</h1>
-    </div>
-  )
-}
+import { Link } from "react-router-dom";
+import { assets, dummyEducatorData } from "../../assets/assets";
+import { useUser, UserButton } from "@clerk/react";
 
-export default Navbar
+const Navbar = () => {
+  const educatorData = dummyEducatorData;
+  const { user } = useUser();
+  return (
+    <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3">
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="w-28 lg:w-32" />
+      </Link>
+      <div className="flex items-center gap-5 text-gray-500 relative">
+        <p>Hi! {user ? user.fullName : "Developers"}</p>
+        {user ? <UserButton /> : <img src={assets.profile_img} className="max-w-8" /> }
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
